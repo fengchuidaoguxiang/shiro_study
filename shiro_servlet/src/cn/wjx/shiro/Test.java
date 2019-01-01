12b1ab8640c5b3de48e798c88c318b516ae1aefe
fengchuidaoguxiang <33154249+fengchuidaoguxiang@users.noreply.github.com>
2019-01-01 08:32:46 +0800

    修改

--- shiro_servlet/src/cn/wjx/shiro/Test.java
+++ shiro_servlet/src/cn/wjx/shiro/Test.java
@@ -4,6 +4,6 @@ import org.apache.shiro.web.env.IniWebEnvironment;
 
 public class Test {
     public static void main(String[] args) {
-        IniWebEnvironment
+//        IniWebEnvironment
     }
 }
